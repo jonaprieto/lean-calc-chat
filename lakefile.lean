e81c56911be19c31e3a71885e4138bab5fa14c1b
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-08 22:29:58 -0500

    build: update terminal to v0.3.1

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «calc-chat» where
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.3.0"
+  @ "v0.3.1"
 
 require «termcolor-diagnostics» from git
   "https://github.com/jonaprieto/lean-termcolor-diagnostics.git"
