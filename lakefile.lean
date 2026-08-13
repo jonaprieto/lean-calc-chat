53cce12cfc06da43fd7cd530e4c1223ee7e7befa
jonaprieto <prieto.jona@gmail.com>
2026-08-13 18:43:30 -0500

    chore(release): prepare release

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «calc-chat» where
-  version := v!"0.5.8"
+  version := v!"0.5.9"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor-terminal» from git
