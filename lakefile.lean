509b45c47246f432ab401f66c82b71701c68153d
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-08 23:03:01 -0500

    build: refresh ecosystem dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -7,31 +7,31 @@ package «calc-chat» where
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.3.6"
+  @ "v0.3.7"
 
 require «termcolor-widgets» from git
   "https://github.com/jonaprieto/lean-termcolor-widgets.git"
-  @ "v0.1.13"
+  @ "v0.1.14"
 
 require «termcolor-diagnostics» from git
   "https://github.com/jonaprieto/lean-termcolor-diagnostics.git"
-  @ "v0.1.16"
+  @ "v0.1.17"
 
 require «grip» from git
   "https://github.com/jonaprieto/lean-grip.git"
-  @ "v0.3.4"
+  @ "v0.3.5"
 
 require «grip-diagnostics» from git
   "https://github.com/jonaprieto/lean-grip-diagnostics.git"
-  @ "v0.2.7"
+  @ "v0.2.8"
 
 require «termcolor-repl» from git
   "https://github.com/jonaprieto/lean-termcolor-repl.git"
-  @ "v0.8.6"
+  @ "v0.8.7"
 
 require argus from git
   "https://github.com/jonaprieto/lean-argus.git"
-  @ "v0.5.3"
+  @ "v0.5.4"
 
 @[default_target]
 lean_lib «Calc» where
